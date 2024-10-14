c0172b5d23b8168320c06d77f459abe1f1720567
Jesse Songok <jessesongok@gmail.com>
2024-10-14 14:49:10 +0300

    Initial Models

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,12 @@ Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
+Route::view('/', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
